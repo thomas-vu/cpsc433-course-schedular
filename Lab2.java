9c112dbf8411ba2de71f2240b39c30798c7a5bb6
Kevin <giangkvn@gmail.com>
2017-11-18 17:25:33 -0700

    Stubbed out Slots.java and finished Assignables, Course, and Lab

---
+++ Lab2.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Lab2 extends Course
+{
+    private int lecture_section = 0;
+
+    public Lab2(String name, int number)
+    {
+        // some lectures only have 1 section, so we don't need to get the lecture section for labs/tuts
+        super(name, number);
+    }
+
+    public Lab2(String name, int number, int lecture)
+    {
+        super(name, number);
+        lecture_section = lecture;
+    }
+
+    public int getLectureSection()
+    {
+        return lecture_section;
+    }
+}
