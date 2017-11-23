459a311bd661943d599e69c5acb1453fef02abce
adrian.bathan <adrian.bathan@zone18-wc.cs.ucalgary.ca>
2017-11-22 20:33:34 -0700

    Div method added to Kevin's unfinished ANDTree

--- ANDTree.java
+++ ANDTree.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 
 public class ANDTree
 {
+	private Random rand = new Random();
     // private ArrayList<Slot> state2 = new ArrayList<Slot>();
-    private ArrayList<HashMap> state = new ArrayList<HashMap>();
+//    private ArrayList<HashMap> state = new ArrayList<HashMap>();
+	private ArrayList<Slot> state = new ArrayList<Slot>();
     // private Asssignable assignment;
     // private Slot assignedTo;
     private ArrayList<Assignable> notAssigned = new ArrayList<Assignable>();
@@ -68,4 +71,27 @@ public class ANDTree
 
         return depth;
     }
+    
+    public ArrayList<ANDTree> div() {
+		int nRemain = notAssigned.length;
+		int nSlots = state.length;
+		if (nRemain < 1) return new ArrayList<ANDTree>();
+		r = rn.nextInt(nRemain);										// r is index of randomly chosen element
+		private ArrayList<ANDTree> childTrees;
+		private Assignable chosen = noAssigned[r]; 						// chooses randomly
+		
+		for (int i=0; i<numStates; i++) {
+			if (state[i].length < state[i].getMax()) {						// if slot is not full
+				if (constraintCheck(state[i], chosen)) {				// IF ASSIGNMENT PASSES CONSTRAINT CHECK // TO BE IMPLEMENTED
+					private ANDTree tempTree = new AndTree(this);		//  initialize a temporary tree
+					tempTree.setState(state[i], chosen);				//  set state of new Tree
+					tempTree.setUnassigned(unassigned.remove(r));		//  set unassigned list of list of unchosen assignables
+					tempTree.setEval(evalFunction);						//	GIVES NEW TREE AN EVAL // TO BE IMPLEMENTED
+					tempTree.setDepth(depth+1);						//	increment depth of new Tree
+					childTrees.add(tempTree);
+				}
+			}
+		}
+		return childTrees;
+	}
 }
